8e79306eee5680255dc27df47e2c03c6a2fe9f68
Praveenkumar-6001 <praveen04072025@gmail.com>
2026-05-28 15:41:22 +0530

    branch created with images

--- src/modules/branches/branches.module.ts
+++ src/modules/branches/branches.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BranchesService } from './branches.service';
 import { BranchesController } from './branches.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { DocumentsModule } from '../documents/documents.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, DocumentsModule],
   controllers: [BranchesController],
   providers: [BranchesService],
   exports: [BranchesService],
